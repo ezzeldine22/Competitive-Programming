8e8c1d989a7417243906d8ab1c848cc6b16a80ba
ezzeldine22 <112328998+ezzeldine22@users.noreply.github.com>
2025-07-21 20:08:42 +0300

    update_#2

---
+++ ECPC Qualifications 2024 Day 2 (Mirror)/Fishing Traditions of Tiberias.cpp
@@ -0,0 +1,42 @@
+// -------------------- اللهم اني اسالك علما نافعا ورزقا طيبا وعملا متقبلا--------------------//
+// ---------------------------اللهم صل وسلم على نبينا محمدﷺ -------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+#define ON(n, k) ((n) | (1 << k))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define IsOn(n, k) (((n) >> (k)) & 1)
+#define ll long long
+#define ull unsigned long long
+#define input freopen("H:/EzzEldine/Computer Science/C++ projects/in.txt", "r", stdin)
+#define output freopen("H:/EzzEldine/Computer Science/C++ projects/out.txt", "w", stdout)
+#define author_EzzEldine         \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+int main()
+{
+    author_EzzEldine
+#ifndef ONLINE_JUDGE
+        input;
+    output;
+#endif
+
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        int n, q;
+        cin >> n;
+        vector<ll> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        cin >> q;
+        while (q--)
+        {
+            
+        }
+        
+    }
+}
