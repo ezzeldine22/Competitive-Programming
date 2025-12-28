cce8819c18d1cf29c44a0f9811cca1e3a3cff565
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-28 21:36:05 +0200

    solve to E_candies problem

---
+++ binary search/E. Candies.cpp
@@ -0,0 +1,44 @@
+// -------------------- اللهم اني اسالك علما نافعا ورزقا طيبا وعملا متقبلا--------------------//
+// ---------------------------اللهم صل وسلم على نبينا محمدﷺ -------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+
+#define author_EzzEldine         \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+int main()
+{
+    author_EzzEldine
+
+#ifndef ONLINE_JUDGE
+        freopen("H:/CS/C++ projects/PrefixSum/input.txt", "r", stdin);
+    freopen("H:/CS/C++ projects/PrefixSum/output.txt", "w", stdout);
+#endif
+
+#include <stdio.h>
+
+    // __int128 a = 12345678901234567890123456789012345678;
+    printf("128-bit integer value\n");
+    return 0;
+
+    // ll n;
+    // cin >> n;
+    // ll l = 1, r = 1e9 * 2, ans = 1;
+    // while (l <= r)
+    // {
+    //     ll mid = (l + r) / 2;
+    //     if (mid * (mid + 1) / 2 <= n)
+    //     {
+    //         ans = mid;
+    //         l = mid + 1;
+    //     }
+    //     else
+    //     {
+    //         r = mid - 1;
+    //     }
+    // }
+    // cout << ans << endl;
+}
