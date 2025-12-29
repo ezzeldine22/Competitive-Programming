1d5196acb5fd99b995bcbf1400e2e277cfba8025
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-29 10:11:29 +0200

    solve to raising bacteria problem

---
+++ bitmask/F. Raising Bacteria.cpp
@@ -0,0 +1,35 @@
+// -------------------- اللهم اني اسالك علما نافعا ورزقا طيبا وعملا متقبلا--------------------//
+// ---------------------------اللهم صل وسلم على نبينا محمدﷺ -------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+#define ON(n, k) ((n) | (1 << k))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define IsOn(n, k) (((n) >> (k)) & 1)
+#define ll long long
+#define ull unsigned long long
+#define input freopen("H:/EzzEldine/Computer Science/C++ projects/PrefixSum/input.txt", "r", stdin)
+#define output freopen("H:/EzzEldine/Computer Science/C++ projects/PrefixSum/output.txt", "w", stdout)
+#define author_EzzEldine         \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+int main()
+{
+    author_EzzEldine
+
+#ifndef ONLINE_JUDGE
+        input;
+    output;
+#endif
+
+    ll x, res{};
+    cin >> x;
+    for (int i = 0; i < 32; i++)
+    {
+        if (IsOn(x, i))
+            res++;
+    }
+    cout << res << endl;
+
+    return 0;
+}
