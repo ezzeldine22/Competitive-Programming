ae9f980a0ad17cd7f2ca07bf606e79df3db2cd03
ezzeldine22 <ezzeldinereda@gmail.com>
2025-12-31 17:24:11 +0200

    solve to detect cycle by bfs problem

---
+++ Graph/H. Detect cycle by bfs.cpp
@@ -0,0 +1,27 @@
+// -------------------- اللهم اني اسالك علما نافعا ورزقا طيبا وعملا متقبلا--------------------//
+// ---------------------------اللهم صل وسلم على نبينا محمدﷺ -------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+#define ON(n, k) ((n) | (1 << k))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define IsOn(n, k) (((n) >> (k)) & 1)
+#define MAX(v) *max_element(v.begin(), v.end())
+#define MIN(v) *min_element(v.begin(), v.end())
+#define ll long long
+#define ull unsigned long long
+#define input freopen("H:/EzzEldine/Computer Science/C++ projects/in.txt", "r", stdin)
+#define output freopen("H:/EzzEldine/Computer Science/C++ projects/out.txt", "w", stdout)
+#define author_EzzEldine         \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+int main()
+{
+    author_EzzEldine
+#ifndef ONLINE_JUDGE
+        input;
+    output;
+#endif
+
+
+}
