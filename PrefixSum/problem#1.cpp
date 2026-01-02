496eccda826cbe6a24d5bfe3f0948562d10717b1
ezzeldine22 <ezzeldinereda@gmail.com>
2026-01-02 15:06:39 +0200

    solve to problem_1 problem

---
+++ PrefixSum/problem#1.cpp
@@ -0,0 +1,23 @@
+// -------------------- اللهم اني اسالك علما نافعا ورزقا طيبا وعملا متقبلا--------------------//
+// ---------------------------اللهم صل وسلم على نبينا محمدﷺ -------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+#define author_EzzEldine         \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+int main()
+{
+    author_EzzEldine
+
+        int a,
+        b;
+    cin >> a >> b;
+    a = (a ^ b);
+    b = (a ^ b);
+    a = (a ^ b);
+
+    cout << a << " " << b;
+}
