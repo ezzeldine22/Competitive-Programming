cfbfc286b8ad1cac07e799c618026e00f9d87e8f
ezzeldine22 <ezzeldinereda@gmail.com>
2026-01-02 15:07:14 +0200

    solve to less or equal problem

---
+++ STL/B. Less or Equal.cpp
@@ -0,0 +1,37 @@
+// -------------------- اللهم اني اسالك علما نافعا ورزقا طيبا وعملا متقبلا--------------------//
+// ---------------------------اللهم صل وسلم على نبينا محمدﷺ -------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+#define author_EzzEldine         \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+int main()
+{
+    author_EzzEldine
+
+#ifndef ONLINE_JUDGE
+        freopen("H:/CS/C++ projects/PrefixSum/input.txt", "r", stdin);
+    freopen("H:/CS/C++ projects/PrefixSum/output.txt", "w", stdout);
+#endif
+
+    int n, k;
+
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    if (k < v.size() && v[k - 1] < v[k] || k == v.size())
+    {
+        cout << v[k - 1];
+    }
+    else
+    {
+        cout << "-1";
+    }
+}
