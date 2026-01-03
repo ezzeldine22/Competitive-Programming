6f8135dbcbb6e53972cc2c0573580857065d155f
ezzeldine22 <ezzeldinereda@gmail.com>
2026-01-03 12:36:55 +0200

    solve to john and swaps problem

---
+++ STL/E. John and Swaps.cpp
@@ -0,0 +1,47 @@
+// -------------------- اللهم اني اسالك علما نافعا ورزقا طيبا وعملا متقبلا--------------------//
+// ---------------------------اللهم صل وسلم على نبينا محمدﷺ -------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+#define author_EzzEldine         \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+int main()
+{
+    author_EzzEldine
+
+#ifndef ONLINE_JUDGE
+        freopen("H:/CS/C++ projects/PrefixSum/input.txt", "r", stdin);
+    freopen("H:/CS/C++ projects/PrefixSum/output.txt", "w", stdout);
+#endif
+
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        int n;
+        vector<int> v;
+        set<vector<int>> seq;
+
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            v.push_back(x);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                swap(v[i], v[j]);
+                seq.insert(v);
+                swap(v[i], v[j]);
+            }
+        }
+
+        cout << seq.size() << endl;
+    }
+}
