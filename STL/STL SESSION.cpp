0bc3d1fc12475c2c9543cefc008a288b8930a925
ezzeldine22 <ezzeldinereda@gmail.com>
2026-01-06 14:26:54 +0200

    solve to stl session problem

---
+++ STL/STL SESSION.cpp
@@ -0,0 +1,23 @@
+// بسم الله نبدا اللهم علمنا بما ينفعنا وانفعنا بما علمتنا وزدنا بك علما
+// صلي علي النبي
+
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> v(10, 1);
+#define ll long long
+
+// Standard Template Library
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("H:/CS/C++ projects/PrefixSum/input.txt", "r", stdin);
+    freopen("H:/CS/C++ projects/PrefixSum/output.txt", "w", stdout);
+#endif
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cout << *v.begin();
+}
