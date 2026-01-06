8779411c1617ecffcdedce6285e6ae2b3b9c2185
ezzeldine22 <ezzeldinereda@gmail.com>
2026-01-06 14:26:32 +0200

    solve to great vova wall problem

---
+++ STL/O. Great Vova Wall (Version 1).cpp
@@ -0,0 +1,18 @@
+// -------------------- اللهم اني اسالك علما نافعا ورزقا طيبا وعملا متقبلا--------------------//
+// ---------------------------اللهم صل وسلم على نبينا محمدﷺ -------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+#define author_EzzEldine         \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+int main()
+{
+    author_EzzEldine
+
+#ifndef ONLINE_JUDGE
+        freopen("H:/CS/C++ projects/PrefixSum/input.txt", "r", stdin);
+    freopen("H:/CS/C++ projects/PrefixSum/output.txt", "w", stdout);
+#endif
+}
