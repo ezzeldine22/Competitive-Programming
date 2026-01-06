eaefd376b06b9570f69f87950d93aebc48be05fd
ezzeldine22 <ezzeldinereda@gmail.com>
2026-01-06 14:29:11 +0200

    solve to maximum subarray problem

---
+++ Two pointers/I. Maximum Subarray Sum.cpp
@@ -0,0 +1,42 @@
+// -------------------- اللهم اني اسالك علما نافعا ورزقا طيبا وعملا متقبلا--------------------//
+// ---------------------------اللهم صل وسلم على نبينا محمدﷺ -------------------------//
+#include <bits/stdc++.h>
+using namespace std;
+#define ON(n, k) ((n) | (1 << k))
+#define OFF(n, k) ((n) & ~(1 << (k)))
+#define IsOn(n, k) (((n) >> (k)) & 1)
+#define ll long long
+#define ull unsigned long long
+#define input freopen("H:/EzzEldine/Computer Science/C++ projects/PrefixSum/input.txt", "r", stdin)
+#define output freopen("H:/EzzEldine/Computer Science/C++ projects/PrefixSum/output.txt", "w", stdout)
+#define author_EzzEldine         \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+
+int main()
+{
+    author_EzzEldine
+#ifndef ONLINE_JUDGE
+        input;
+    output;
+#endif
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    int l = 0, r = 0, ans{}, max_ans{};
+
+    while (l < n)
+    {
+
+        while (r < n && ans + v[r] > max_ans)
+        {
+        }
+    }
+}
